da3736d8931d42345aa9d5b13020b2ab20a98d62
Bonanoo <61703768+AnyBananaGAME@users.noreply.github.com>
2025-06-28 19:46:07 +0300

    Create Bool.zig
    Bool struct for bool I/O

---
+++ src/types/unsigned/Bool.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const BinaryStream = @import("../../stream/BinaryStream.zig").BinaryStream;
+const Uint8 = @import("../../types/unsigned/UInt8.zig").Uint8;
+
+pub const Bool = struct {
+    pub fn write(stream: *BinaryStream, value: bool) void {
+        Uint8.write(stream, if (value) 1 else 0);
+    }
+
+    pub fn read(stream: *BinaryStream) bool {
+        return Uint8.read(stream) == 1;
+    }
+};
+
+test "Bool read/write" {
+    std.debug.print("Running test: Bool read/write\n", .{});
+    var buffer: [10]u8 = [_]u8{0} ** 10;
+    var stream = BinaryStream.init(std.testing.allocator, &buffer, 0);
+    defer stream.deinit();
+
+    // Test writing a value
+    const test_value: bool = true;
+    Bool.write(&stream, test_value);
+
+    // Reset offset to read from the beginning
+    stream.offset = 0;
+
+    // Test reading the value
+    const read_value = Bool.read(&stream);
+    try std.testing.expectEqual(test_value, read_value);
+}
