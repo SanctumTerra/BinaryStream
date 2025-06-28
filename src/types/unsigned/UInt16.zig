27f7e0ce216688e5a30239718fabae2392088d02
Bonanoo <61703768+AnyBananaGAME@users.noreply.github.com>
2025-06-28 19:45:18 +0300

    Create UInt16.zig
    Uint16 struct for u16 I/O

---
+++ src/types/unsigned/UInt16.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+const BinaryStream = @import("../../stream/BinaryStream.zig").BinaryStream;
+const Endianess = @import("../../enums/Endianess.zig").Endianess;
+
+pub const Uint16 = struct {
+    pub fn write(stream: *BinaryStream, value: u16, endianess: ?Endianess) void {
+        var bytes: [2]u8 = undefined;
+        switch (endianess orelse .Big) {
+            .Little => {
+                bytes[0] = @intCast(value & 0xFF);
+                bytes[1] = @intCast((value >> 8) & 0xFF);
+            },
+            .Big => {
+                bytes[0] = @intCast((value >> 8) & 0xFF);
+                bytes[1] = @intCast(value & 0xFF);
+            },
+        }
+        stream.write(&bytes);
+    }
+
+    pub fn read(stream: *BinaryStream, endianess: ?Endianess) u16 {
+        const bytes = stream.read(2);
+        if (bytes.len < 2) {
+            std.log.err("Cannot read uint16: not enough bytes", .{});
+            return 0;
+        }
+        return switch (endianess orelse .Big) {
+            .Little => @as(u16, @intCast(bytes[0])) | (@as(u16, @intCast(bytes[1])) << 8),
+            .Big => (@as(u16, @intCast(bytes[0])) << 8) | @as(u16, @intCast(bytes[1])),
+        };
+    }
+};
+
+test "Uint16 read/write" {
+    std.debug.print("Running test: Uint16 read/write\n", .{});
+    var buffer: [10]u8 = [_]u8{0} ** 10;
+    var stream = BinaryStream.init(std.testing.allocator, &buffer, 0);
+    defer stream.deinit();
+
+    // Test writing a value
+    const test_value: u16 = 42;
+    Uint16.write(&stream, test_value, .Big);
+
+    // Reset offset to read from the beginning
+    stream.offset = 0;
+
+    // Test reading the value
+    const read_value = Uint16.read(&stream, .Big);
+    try std.testing.expectEqual(test_value, read_value);
+}
